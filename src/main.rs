b21ecfa492ac31a9433a231bc2e83d6b84da0e0d
Benoit Petit <bpetit@hubblo.org>
2020-10-29 15:08:08 +0100

    chore: ensure at least one subcommand is supplied

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 //! Generic sensor and transmission agent for energy consumption related metrics. 
 //! 
-use clap::{Arg, App, SubCommand};
+use clap::{Arg, App, SubCommand, AppSettings};
 use scaphandre::{run, get_exporters_options};
 
 fn main() {
@@ -15,6 +15,7 @@ fn main() {
         .author("Benoit Petit <bpetit@hubblo.org>")
         .version("0.1.0")
         .about("Agnostic software sensor and data collection agent for energy/electricity consumption related metrics")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
         .arg(
             Arg::with_name("sensor")
                 .value_name("sensor")
@@ -26,17 +27,6 @@ fn main() {
                 .short("s")
                 .long("sensor")
         );
-        //.arg(
-        //    Arg::with_name("exporter")
-        //        .value_name("exporter")
-        //        .help("The exporter module to apply on the host to get energy consumption metrics.")
-        //        .required(true)
-        //        .takes_value(true)
-        //        .possible_values(&res)
-        //        .default_value("stdout")
-        //        .short("e")
-        //        .long("exporter")
-        //);
     for exp in res {
         let mut subcmd = SubCommand::with_name(exp);
         for (key, opt) in exporters_options.get(exp).unwrap().iter() {
@@ -51,6 +41,5 @@ fn main() {
         }
         matches = matches.subcommand(subcmd);
     }
-        //TODO: find a way to dynamically add exporters options in a distinct category for each exporter
     run(matches.get_matches());
 }
