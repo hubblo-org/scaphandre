33a6270e18c14fa9820ade599c3f615dc2899467
Uggla <uggla@free.fr>
2020-12-31 17:37:30 +0100

    fix: Clean up to remove warnings

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,5 +1,4 @@
-use clap::App;
-use scaphandre::exporters::{qemu::QemuExporter, Exporter};
+use scaphandre::exporters::qemu::QemuExporter;
 use scaphandre::sensors::powercap_rapl::PowercapRAPLSensor;
 use std::fs;
 
@@ -7,13 +6,8 @@ use std::fs;
 fn exporter_qemu() {
     let sensor = PowercapRAPLSensor::new(1, 1, false);
     let mut exporter = QemuExporter::new(Box::new(sensor));
-    let parameters = App::new("scaphandre").get_matches();
-    //exporter.run(parameters);
     let path = "/var/lib/libvirt/scaphandre/integration_tests";
     exporter.iteration(String::from(path));
     let content = fs::read_dir(path);
     assert_eq!(content.is_ok(), true);
-    //for subfolder in content.unwrap() {
-    //
-    //}
 }
