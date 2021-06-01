e34780b48392a8a95a2cc807c970a53bb661501f
Benoit Petit <bpetit@nce.re>
2021-06-01 13:37:12 +0200

    r(prometheus)efacto: first working version with tokio/hyper, no metrics yet

--- src/exporters/prometheus.rs
+++ src/exporters/prometheus.rs
@@ -7,11 +7,12 @@ use crate::sensors::{Sensor, Topology};
 use crate::exporters::Exporter;
 use chrono::Utc;
 use clap::{Arg, ArgMatches};
-use std::{collections::HashMap, net::SocketAddr, time::Duration, sync::Mutex};
+use std::{collections::HashMap, net::{IpAddr, SocketAddr}, str::FromStr, sync::Arc, time::Duration};
 use super::utils::get_hostname;
 use std::convert::Infallible;
 use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
+use hyper::server::conn::AddrStream;
 
 /// Default ipv4/ipv6 address to expose the service is any
 const DEFAULT_IP_ADDRESS: &str = "::";
@@ -42,18 +43,15 @@ impl Exporter for PrometheusExporter {
         );
         println!("Press CTRL-C to stop scaphandre");
 
-        //match runner(
-        //    (*self.sensor.get_topology()).unwrap(),
-        //    parameters.value_of("address").unwrap().to_string(),
-        //    parameters.value_of("port").unwrap().to_string(),
-        //    parameters.value_of("suffix").unwrap().to_string(),
-        //    parameters.is_present("qemu"),
-        //    parameters.is_present("containers"),
-        //    get_hostname(),
-        //) {
-        //    Ok(()) => warn!("Prometheus exporter shut down gracefully."),
-        //    Err(error) => panic!("Something failed in the prometheus exporter: {}", error),
-        //}
+        runner(
+            (*self.sensor.get_topology()).unwrap(),
+            parameters.value_of("address").unwrap().to_string(),
+            parameters.value_of("port").unwrap().to_string(),
+            parameters.value_of("suffix").unwrap().to_string(),
+            parameters.is_present("qemu"),
+            parameters.is_present("containers"),
+            get_hostname(),
+        );
     }
     /// Returns options understood by the exporter.
     fn get_options() -> Vec<clap::Arg<'static, 'static>> {
@@ -107,11 +105,49 @@ impl Exporter for PrometheusExporter {
 /// Contains a mutex holding a Topology object.
 /// Used to pass the topology data from one http worker to another.
 struct PowerMetrics {
-    topology: Mutex<Topology>,
-    last_request: Mutex<Duration>,
+    topology: Topology,
+    last_request: Duration,
     qemu: bool,
     containers: bool,
     hostname: String,
+}
+
+#[tokio::main]
+async fn runner(
+    topology: Topology, address: String, port: String, suffix: String, qemu: bool, containers: bool, hostname: String,
+){
+    if let Ok(addr) = address.parse::<IpAddr>() {
+        if let Ok(port) = port.parse::<u16>() {
+            let socket_addr = SocketAddr::new(addr, port);
+            let context = Arc::new(PowerMetrics {
+                topology: topology.clone(),
+                last_request: Duration::new(0, 0),
+                qemu,
+                containers,
+                hostname: hostname.clone(),
+            });
+            let make_svc = make_service_fn(move |_| {
+                async {
+                    Ok::<_, Infallible>(
+                            service_fn( move |req| {
+                                show_metrics(req)
+                            }
+                        )
+                    )
+                }
+            });
+            let server = Server::bind(&socket_addr);
+            let res = server.serve(make_svc);
+
+            if let Err(e) = res.await {
+                error!("server error: {}", e);
+            }
+        } else {
+            panic!("{} is not a valid TCP port number", port);
+        }
+    } else {
+        panic!("{} is not a valid ip address", address);
+    }
 }
 
 //#[actix_web::main]
@@ -150,36 +186,57 @@ struct PowerMetrics {
 //    .await
 //}
 //
-///// Returns a well formatted Prometheus metric string.
-//fn format_metric(key: &str, value: &str, labels: Option<&HashMap<String, String>>) -> String {
-//    let mut result = key.to_string();
-//    if let Some(labels) = labels {
-//        result.push('{');
-//        for (k, v) in labels.iter() {
-//            result.push_str(&format!("{}=\"{}\",", k, v));
-//        }
-//        result.remove(result.len() - 1);
-//        result.push('}');
-//    }
-//    result.push_str(&format!(" {}\n", value));
-//    result
-//}
+/// Returns a well formatted Prometheus metric string.
+fn format_metric(key: &str, value: &str, labels: Option<&HashMap<String, String>>) -> String {
+    let mut result = key.to_string();
+    if let Some(labels) = labels {
+        result.push('{');
+        for (k, v) in labels.iter() {
+            result.push_str(&format!("{}=\"{}\",", k, v));
+        }
+        result.remove(result.len() - 1);
+        result.push('}');
+    }
+    result.push_str(&format!(" {}\n", value));
+    result
+}
+
+/// Adds lines related to a metric in the body (String) of response.
+fn push_metric(
+    mut body: String,
+    help: String,
+    metric_type: String,
+    metric_name: String,
+    metric_line: String,
+) -> String {
+    body.push_str(&format!("# HELP {} {}", metric_name, help));
+    body.push_str(&format!("\n# TYPE {} {}\n", metric_name, metric_type));
+    body.push_str(&metric_line);
+    body
+}
+
+//#[derive(Clone, Copy)]
+//struct Router {
 //
-///// Adds lines related to a metric in the body (String) of response.
-//fn push_metric(
-//    mut body: String,
-//    help: String,
-//    metric_type: String,
-//    metric_name: String,
-//    metric_line: String,
-//) -> String {
-//    body.push_str(&format!("# HELP {} {}", metric_name, help));
-//    body.push_str(&format!("\n# TYPE {} {}\n", metric_name, metric_type));
-//    body.push_str(&metric_line);
-//    body
 //}
-//
-///// Handles requests and returns data formated for Prometheus.
+
+//impl Router {
+    /// Handles requests and returns data formated for Prometheus.
+    async fn show_metrics(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+        warn!("{}", req.uri());
+        let mut body = String::new();
+        if req.uri().path() == "/metrics" {
+            body.push_str("Here come tha metriczzz !!!");
+        } else {
+            body.push_str("go to /metrics !!");
+        }
+        Ok(Response::new(body.into()))
+    }
+
+//}
+//async fn show_metrics(context: Arc<PowerMetrics>, req: Request<Body>) -> Result<Response<Body>, Infallible> {
+//    Ok(Response::new("Coucou toi !".into()))
+//}
 //async fn show_metrics(data: web::Data<PowerMetrics>) -> impl Responder {
 //    let now = current_system_time_since_epoch();
 //    let mut last_request = data.last_request.lock().unwrap();
