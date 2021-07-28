e91953814679b3c1ee03d7e939da73f062c0651f
Benoit Petit <bpetit@nce.re>
2021-07-28 09:27:09 +0200

    fix: escaping enforced double commas in commandline

--- src/exporters/prometheus.rs
+++ src/exporters/prometheus.rs
@@ -171,7 +171,7 @@ fn format_metric(key: &str, value: &str, labels: Option<&HashMap<String, String>
     if let Some(labels) = labels {
         result.push('{');
         for (k, v) in labels.iter() {
-            result.push_str(&format!("{}=\"{}\",", k, v));
+            result.push_str(&format!("{}=\"{}\",", k, v.replace("\"", "_")));
         }
         result.remove(result.len() - 1);
         result.push('}');
