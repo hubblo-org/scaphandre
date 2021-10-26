4e4b8e64ce6f26e49ab6ec86b0f458d4c6da1f03
Ross Fairbanks <ross@rossfairbanks.com>
2021-10-26 13:44:42 +0200

    fix: support k8s pods using containerd

--- src/sensors/utils.rs
+++ src/sensors/utils.rs
@@ -257,6 +257,8 @@ impl ProcessTracker {
                                         Some(id) => {
                                             if let Some(final_id) = id.strip_prefix("docker://") {
                                                 final_id == container_id
+                                            } else if let Some(final_id) = id.strip_prefix("containerd://") {
+                                                final_id == container_id
                                             } else {
                                                 false
                                             }
