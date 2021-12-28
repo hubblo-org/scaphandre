1fef704c83f96b4037afde99a85a55940002b4b0
bpetit <bpetit@hubblo.org>
2021-12-28 18:30:48 +0100

    build: escaped linux specific code

--- src/exporters/mod.rs
+++ src/exporters/mod.rs
@@ -740,6 +740,7 @@ impl MetricGenerator {
             if let Some(cmdline_str) = cmdline {
                 attributes.insert("cmdline".to_string(), cmdline_str.replace("\"", "\\\""));
 
+                #[cfg(target_os="linux")]
                 if self.qemu {
                     if let Some(vmname) = utils::filter_qemu_cmdline(&cmdline_str) {
                         attributes.insert("vmname".to_string(), vmname);
