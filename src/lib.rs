e32a21dd2336998d7c03a124270d314220f7fe18
Guillaume Raffin <theelectronwill@gmail.com>
2023-05-22 17:03:25 +0200

    Remove broken python bindings

--- src/lib.rs
+++ src/lib.rs
@@ -36,11 +36,6 @@ fn current_system_time_since_epoch() -> Duration {
         .unwrap()
 }
 
-/// Returns rust crate version, can be use used in language bindings to expose Rust core version
-pub fn crate_version() -> &'static str {
-    env!("CARGO_PKG_VERSION")
-}
-
 //  Copyright 2020 The scaphandre authors.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
